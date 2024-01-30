cbd1c429e9b0cf1e8857f07b033662f56c472ec2
cagalo11 <garcia_camilo@rocketmail.com>
2024-01-30 12:49:39 -0500

    Pruebas para ejecucion 2

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+import sys
 
-def pares(lista, objetivo):
-  resultados = []
-  minimo_valor = min(lista)
-  maximo_valor = max(lista)
-  rango = maximo_valor - minimo_valor
-  lista = range(minimo_valor, maximo_valor)
-  for i in lista:
-    for j in lista:
-      if lista[i] + lista[j] == objetivo:
-        resultados.append(str(lista[i]) + ","  +  str(lista[j]))
-  return resultados
+def pares(lista=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], objetivo=5):
+    
+    resultados = []
+    for i in range(len(lista)):
+        for j in range(i+1, len(lista)): 
+            if lista[i] + lista[j] == objetivo:
+                resultados.append(f"{lista[i]},{lista[j]}")
+    return resultados
+
+if __name__ == "__main__":
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # Default list
+    objetivo = 5  # Default objetivo
+
+    if len(sys.argv) > 1:
+        # Assuming command-line 'lista' as comma-separated values, e.g., "1,2,3,4,5"
+        lista = list(map(int, sys.argv[1].split(',')))
+    if len(sys.argv) > 2:
+        objetivo = int(sys.argv[2])
+
+    print(pares(lista, objetivo))
--- borrar5.py
+++ borrar5.py
@@ -1,5 +1,14 @@
+import sys
+
 def ejemplo(num):
     if num == 5:
         print("correcto")
     else:
         print("el numero es diferente a 5")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        num = int(sys.argv[1])
+        ejemplo(num)
+    else:
+        print("Please provide a number as an argument.")
